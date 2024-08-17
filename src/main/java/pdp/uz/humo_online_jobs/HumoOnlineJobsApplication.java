9be091c89a67a0313f886038aa8a6b5c225aa555
stranger <ismoiljonovabdulaziz97@gmail.com>
2024-08-17 21:57:48 +0500

    Update

--- src/main/java/pdp/uz/humo_online_jobs/HumoOnlineJobsApplication.java
+++ src/main/java/pdp/uz/humo_online_jobs/HumoOnlineJobsApplication.java
@@ -2,12 +2,21 @@ package pdp.uz.humo_online_jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class HumoOnlineJobsApplication {
+public class HumoOnlineJobsApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(HumoOnlineJobsApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
